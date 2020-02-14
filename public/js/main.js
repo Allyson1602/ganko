86e7103a102ca215aa81e1e1a839539114d45778
Allyson Athyrson <athyrsonallyson@gmail.com>
2020-02-14 11:07:47 -0300

    design painel msg

---
+++ public/js/main.js
@@ -0,0 +1,6 @@
+var modal = document.getElementById('content-modal');
+var span  = document.getElementsByClassName('close')[0];
+
+function fechar(){
+    document.getElementById('content-modal').style.display = "none";
+}
